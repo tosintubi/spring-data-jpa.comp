6f79a514ba5efd783741a37302aa327d2ef0b31d
Tosin <tosintubi@users.noreply.github.com>
2021-07-18 22:35:13 +0200

    initial commit

---
+++ src/main/java/com/tommot/springdatajpa/comp/Application.java
@@ -0,0 +1,13 @@
+package com.tommot.springdatajpa.comp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
