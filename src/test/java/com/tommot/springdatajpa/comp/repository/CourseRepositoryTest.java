127f459329ec44e7ab99dfff292859c8c4dfd817
Tosin <tosintubi@users.noreply.github.com>
2021-07-20 21:24:43 +0200

    test Course cascading from CourseRepository

---
+++ src/test/java/com/tommot/springdatajpa/comp/repository/CourseRepositoryTest.java
@@ -0,0 +1,23 @@
+package com.tommot.springdatajpa.comp.repository;
+
+import com.tommot.springdatajpa.comp.entity.Course;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class CourseRepositoryTest {
+
+    @Autowired
+    private CourseRepository courseRepository;
+
+    @Test
+    public void findAllCourses(){
+        List<Course> courses = courseRepository.findAll();
+        courses.forEach(System.out::println);
+    }
+}
