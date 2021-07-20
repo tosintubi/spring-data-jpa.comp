a64929249e29d8c0fd4542f5a005fbbd8cafb793
Tosin <tosintubi@users.noreply.github.com>
2021-07-20 20:20:27 +0200

    Course and CourseMaterial entity

---
+++ src/main/java/com/tommot/springdatajpa/comp/entity/Course.java
@@ -0,0 +1,31 @@
+package com.tommot.springdatajpa.comp.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@AllArgsConstructor
+@Data
+@NoArgsConstructor
+@Builder
+@Table(name = "tbl_course")
+public class Course {
+    @Id
+    @SequenceGenerator(
+            name = "course_seq",
+            sequenceName = "course_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "course_seq"
+    )
+    private Long courseId;
+    private String title;
+    private Integer courseTitle;
+
+}
---
+++ src/main/java/com/tommot/springdatajpa/comp/entity/CourseMaterial.java
@@ -0,0 +1,36 @@
+package com.tommot.springdatajpa.comp.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@AllArgsConstructor
+@Data
+@NoArgsConstructor
+@Builder
+@Table(name = "tbl_course_material")
+public class CourseMaterial {
+    @Id
+    @SequenceGenerator(
+            name = "course_material_seq",
+            sequenceName = "course_material_seq",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.IDENTITY,
+            generator = "course_material_sequence"
+    )
+    private Long courseMaterialId;
+    private String url;
+
+    @OneToOne
+    @JoinColumn(
+            name = "course_id", // from Course Table
+            referencedColumnName = "courseId"
+    )
+    private Course course;
+}
