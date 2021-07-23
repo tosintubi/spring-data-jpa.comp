e8991b59df4367f9aacec8dc6119546be3968a2c
Tosin <tosintubi@users.noreply.github.com>
2021-07-23 23:37:08 +0200

    cascading ALL to fix InvalidDataAccessApiUsageException

--- src/main/java/com/tommot/springdatajpa/comp/entity/Course.java
+++ src/main/java/com/tommot/springdatajpa/comp/entity/Course.java
@@ -44,7 +44,9 @@ public class Course {
     private Teacher teacher;
 
 
-    @ManyToMany
+    @ManyToMany(
+            cascade = CascadeType.ALL
+    )
     @JoinTable(
             name = "student_course_map",
             joinColumns = @JoinColumn(
