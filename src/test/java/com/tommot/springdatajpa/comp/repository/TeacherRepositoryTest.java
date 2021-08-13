75569c501cf6356528fbc5543c273322b5ae8dcb
Tosin <tosintubi@users.noreply.github.com>
2021-08-13 15:24:45 +0200

    unused imports cleanup

--- src/test/java/com/tommot/springdatajpa/comp/repository/TeacherRepositoryTest.java
+++ src/test/java/com/tommot/springdatajpa/comp/repository/TeacherRepositoryTest.java
@@ -6,10 +6,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import javax.accessibility.AccessibleText;
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class TeacherRepositoryTest {
